ec2487ac62a8039fbbf5bdb3c07fc70729d96242
gwon056-hash <gwon056@gmail.com>
2026-07-04 16:46:02 +0900

    Add previous chart search

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moa-charting-2026.07.04-1';
+const CACHE_NAME = 'moa-charting-2026.07.04-2';
 const APP_SHELL = [
   './',
   './index.html',
@@ -32,7 +32,6 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   const req = event.request;
   if (req.method !== 'GET') return;
-
   const url = new URL(req.url);
 
   if (req.mode === 'navigate' || url.pathname.endsWith('/index.html')) {
